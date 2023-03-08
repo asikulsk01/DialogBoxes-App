1c4315fbcf21433a197635b2033f45138e0b0e48
ashikulsk01 <ashikulsk.01@gmail.com>
2023-03-09 00:16:30 +0530

    Splash screen added

---
+++ app/src/main/java/com/example/dialogboxes/SplashScreenActivity.kt
@@ -0,0 +1,26 @@
+package com.example.dialogboxes
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.core.os.postDelayed
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            // Your Code
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1000)
+
+
+
+    }
+}
